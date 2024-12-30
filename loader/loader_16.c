6bd044b2d8b6059e74df073f7c917e261ac846f1
lyra <365373011@qq.com>
2024-12-30 13:34:40 +0800

    修复kernel加载bug

--- loader/loader_16.c
+++ loader/loader_16.c
@@ -47,7 +47,6 @@ static void enter_protect_mode(void)
     write_cr0(cr0);
     // 0x08 是代码段选择子 远跳转清空流水线
     far_jump((unint32_t)8, (unint32_t)protected_mode_entry);
-    show_msg("protect mode done.");
 }
 
 // 读取内存信息
