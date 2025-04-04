1569de6e86bb43ee03fea270d38c7a51ca931524
lyra <365373011@qq.com>
2025-04-04 20:18:14 +0800

    修改启动脚本

--- script/qemu-debug.sh
+++ script/qemu-debug.sh
@@ -1,3 +1,3 @@
-export PROJECT_HOME_PATH=/home/lyra/os/lyra-linux
+export PROJECT_HOME_PATH=/home/lyra/os/lyra-linux-os
 
 qemu-system-i386 -daemonize -m 128M -drive file=$PROJECT_HOME_PATH/image/disk1.img,format=raw,index=0,media=disk -s -S -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
